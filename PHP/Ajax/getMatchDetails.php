19dd8ae82e40f770fa85b087bce6bee7363a4256
Benjamin Christau <benjamin.christau@oyez.fr>
2016-05-11 11:26:47 +0200

    [MatchDetails] First implementation

--- PHP/Ajax/getMatchDetails.php
+++ PHP/Ajax/getMatchDetails.php
@@ -1,7 +1,11 @@
 <?php
+	include_once "../Includes/LOLApi.php";
+	include_once "../Includes/database.php";
 	include_once "../Includes/util.php";
 	include_once "../Includes/simple_html_dom.php";
-		
+
+	session_start();
+
 	if(isset($_GET["matchID"]))
 	{		
 		$matchID = $_GET["matchID"];
@@ -11,7 +15,37 @@
 		$url = "https://euw.api.pvp.net/api/lol/euw/v2.2/match/".$matchID."?api_key=84437920-ce89-4491-97bd-df592330ab93";
 		$json = json_decode(@file_get_contents($url), true);
 		if(!empty($json)) {
-			echo($json);
+
+			if(isset($_SESSION['LOLApi'])){
+				$LOLApi = new LOLApi($_SESSION['LOLApi']);
+
+				$match_playerID = null;
+				$win = null;
+				foreach ($json['participantIdentities'] as $key => $value) {
+					if($value["player"]["summonerId"] == $LOLApi->getSummonerID())
+						$match_playerID = $value["participantId"];
+				}
+
+				foreach ($json['participants'] as $key => $value) {
+					if($value["participantId"] == $match_playerID)
+						$win = $value["stats"]["winner"];
+				}
+
+				$match["win"] = $win;
+
+				// Résultats
+				$response_array['status'] = "success";
+				$response_array['match'] = $match;
+				echo json_encode($response_array);
+			}
+			else
+			{
+				// Résultats
+				$response_array['status'] = "error";
+				$response_array['message'] = "Session error";
+				echo json_encode($response_array);
+				exit();
+			}
 		}
 		else 
 		{
@@ -21,11 +55,6 @@
 			echo json_encode($response_array);
 			exit();
 		}
-		
-		// Résultats
-		$response_array['status'] = "success";
-		$response_array['matches'] = $matches;
-		echo json_encode($response_array);
 	}
 	else
 	{
