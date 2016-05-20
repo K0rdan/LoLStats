250a14607c3b688143deecef286a2a2e2a5d5a53
Benjamin Christau <benjamin.christau@oyez.fr>
2016-05-20 16:55:24 +0200

    [Server] handle pause/resume queue and errors on the socket.

--- JS/server.js
+++ JS/server.js
@@ -14,38 +14,19 @@
 
 const PORT = 8080;
 
-var lastQuery = 0;
-var queue = async.queue(function(params, callback){
-    //console.log("Simulate HTTP GET for : ",params.req);
-
-    try{
-        if(lastQuery == 0){
-            executeRequest(params, callback);
-            lastQuery = Date.now();
-        }
-        else{
-            if( (Date.now() - lastQuery) < LOLApi.queryRateLimit() ){
-                while((Date.now() - lastQuery) < LOLApi.queryRateLimit());
-                //console.log((Date.now() - lastQuery) + "ms awaited");
-                executeRequest(params, callback);
-                lastQuery = Date.now();
-            }
-        }
-    }
-    catch(e) {
-        console.log("[ERROR] - ",e);
-    }
-});
-queue.drain = function(){
-    console.log("Queue is empty");
-}
-
+var queue = initQueue();
 var server = http.createServer();
+var clients = [];
 var io = require('socket.io').listen(server);
+
 io.sockets.on('connection', function(socket){
     console.log(' - New connection from ' + socket.request.connection.remoteAddress);
 
     var _LOLApi = new LOLApi();
+    var pausedTasks = [];
+
+    // Save the client
+    clients.push(socket.id);
 
     socket.emit('message', 'connected');
     socket.on('setSummonerID', function(summonerID){
@@ -61,6 +42,44 @@ io.sockets.on('connection', function(socket){
             _LOLApi.getMatchDetails(queue, match, socket);
         }, this);
     });
+    socket.on('pauseQueue', function(){
+        if(queue.length() > 0){
+            queue.pause();
+
+            // Inverted loop to not affect the index (i) when we splice the array.
+            for(var i=queue.length()-1; i >= 0; i--){
+                if(queue.tasks[i].data.user == socket){
+                    pausedTasks.push({"matchId" : queue.tasks[i].data.match});// Add the task into the pausedTasks array
+                    queue.tasks.splice(i, 1);
+                }
+            }
+
+            queue.resume();
+            socket.emit('message', 'Tasks paused');
+        }
+        else
+            socket.emit('message', 'Queue empty');
+    });
+    socket.on('resumeQueue', function(){
+        if(pausedTasks.length > 0){
+            if(queue.idle()){
+                queue = initQueue();
+            }
+
+            // Inverted loop to not affect the index (i) when we splice the array.
+            for(var i=pausedTasks.length-1; i >= 0; i--){
+                _LOLApi.getMatchDetails(queue, pausedTasks[i], socket);
+                pausedTasks.splice(i, 1);
+            }
+
+            socket.emit('message', 'Tasks resumed');
+        }
+        else
+            socket.emit('message', 'Queue Empty');
+    });
+    socket.on('error', function(err){
+       console.log("[ERROR] - " + err);
+    });
 });
 
 server.listen(PORT);
@@ -71,6 +90,36 @@ console.log("The LOLStats server is now listening on " + os.hostname() + ":" + P
 
 
 ///////
+function initQueue(){
+    var lastQuery = 0;
+    var q = async.queue(function(params, callback){
+        try{
+            console.log("Simulate HTTP GET for : ",params.req);
+
+            if(lastQuery == 0){
+                executeRequest(params, callback);
+                lastQuery = Date.now();
+            }
+            else{
+                if( (Date.now() - lastQuery) < LOLApi.queryRateLimit() ){
+                    while((Date.now() - lastQuery) < LOLApi.queryRateLimit());
+                    //console.log((Date.now() - lastQuery) + "ms awaited");
+                    executeRequest(params, callback);
+                    lastQuery = Date.now();
+                }
+            }
+        }
+        catch(e) {
+            console.log("[ERROR] - ",e);
+        }
+    });
+    q.drain = function(){
+        console.log("Queue is empty");
+        initQueue();
+    };
+
+    return q;
+}
 function executeRequest(params, callback){
     var jsonObj = '';
     var parsedUrl = url.parse(params.req);
